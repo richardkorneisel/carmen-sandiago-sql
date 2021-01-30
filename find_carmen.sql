1fd61635147e99d1e1fc7e7e3c052df976a31de0
Rich Korneisel <korneisel@cfu.net>
2021-01-29 19:05:29 -0600

    Clue 2

--- find_carmen.sql
+++ find_carmen.sql
@@ -7,8 +7,8 @@ select name, region, population from country where region = 'Southern Europe' or
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
-
+select * from countrylanguage where countrycode = 'VAT'
+"VAT"	"Italian"	true	0
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
