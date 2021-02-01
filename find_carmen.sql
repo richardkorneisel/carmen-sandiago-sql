d532edd8f5810e2038d2485c7555e5ee2c067b73
Rich Korneisel <korneisel@cfu.net>
2021-01-31 18:07:01 -0600

    Change to 1 with one search vs two

--- find_carmen.sql
+++ find_carmen.sql
@@ -3,7 +3,8 @@
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 select name, region, population from country where region = 'Southern Europe' order by population;
 "Holy See (Vatican City State)"	"Southern Europe"	1000
-
+More specific search
+select name, region, population from country where region = 'Southern Europe' order by population limit 1;
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
